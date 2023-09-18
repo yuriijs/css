eb1992e030ffe48bcecf37eb39a6c42c5fcec4e2
yuriijs <polevichx@gmail.com>
2023-09-18 11:00:38 +0300

    5.22

--- src/route/index.js
+++ src/route/index.js
@@ -769,6 +769,18 @@ router.get('/template-5', function (req, res) {
 		{
 		  title: "Loyalty Program",
 		  text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
+		},
+		{
+			title: "24/7 Availability",
+			text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the."
+		},
+		{
+			title: "10 Years of Expertise",
+			text: "Fill out all required fields using real data on the checkout page."
+		},
+		{
+			title: "Loyalty Program",
+			text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
 		}
 	  ],
   
